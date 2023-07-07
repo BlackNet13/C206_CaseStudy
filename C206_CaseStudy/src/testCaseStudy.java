a2190288790be623a7178186f51e06259eeb216c
BlackNet13 <123053395+BlackNet13@users.noreply.github.com>
2023-07-08 00:01:25 +0800

    created test file for testing codes before implementing

---
+++ C206_CaseStudy/src/testCaseStudy.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class testCaseStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//conduct tests here:
+		/*example1:
+		 * 
+		 * Ward[] wardArr = new Ward[4];
+		 * 
+		 * wardArr[0] = new Ward("A","ER Patients",20,100.00);
+		 * 
+		 * 
+		 * -------------------
+		 * 
+		 * example2:
+		 * 
+		 * ArrayList<Patient> patientList = new ArrayList<Patient>();
+		 * 
+		 * patientList.add(new Patient("123U","John1","A",5,"03/01/2022","04/02/2022",0,1));
+		 * patientList.add(new Patient("123K","John2","B1",5,"03/01/2022",2));
+		 * 
+		 * 
+		 * Patient p1 = patientList.get(1); 
+		 * p1.display();
+		 * patientList.get(1).display();
+		 * 
+		 */
+
+	}
+
+}
