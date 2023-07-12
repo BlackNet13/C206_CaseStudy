f7096579bb16cc620c6572c3eed5b29dcaae10b7
22002222 <22002222@myrp.edu.sg>
2023-07-12 18:52:41 +0800

    application

---
+++ C206_CaseStudy/src/Applications.java
@@ -0,0 +1,62 @@
+//Marcus
+public class Applications {
+private int appId;
+private int activityId;
+private String studentId;
+private String status;
+
+public Applications(int appId, int activityId, String studentId, String status ) {
+	this.appId = appId;
+	this.activityId = activityId;
+	this.studentId = studentId;
+	this.status = status;
+}
+
+public int getAppId() {
+	return appId;
+}
+
+public void setAppId(int appId) {
+	this.appId = appId;
+}
+
+public int getActivityId() {
+	return activityId;
+}
+
+public void setActivityId(int activityId) {
+	this.activityId = activityId;
+}
+
+public String getStudentId() {
+	return studentId;
+}
+
+public void setStudentId(String studentId) {
+	this.studentId = studentId;
+}
+
+public String getStatus() {
+	return status;
+}
+
+public void setStatus(String status) {
+	this.status = status;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+}
