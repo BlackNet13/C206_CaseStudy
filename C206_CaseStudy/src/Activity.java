5363b332643f73481cdd936c4d3ff772ca766774
masedryer <22020860@myrp.edu.sg>
2023-07-12 23:16:13 +0800

    updated Activity Class

---
+++ C206_CaseStudy/src/Activity.java
@@ -0,0 +1,43 @@
+
+public class Activity {
+	
+private int activityID;
+private String type;
+private int totalCCAdays;
+private int maxPax;
+private String status;
+private int teacherID;
+
+
+public Activity(int activityID, String type , int totalCCAdays , int maxPax, String status, int teacherID) {
+	this.activityID = activityID;
+	this.type = type;
+	this.totalCCAdays = totalCCAdays;
+	this.maxPax = maxPax;
+	this.status = status;
+	this.teacherID = teacherID;
+	
+	
+}
+
+public int getActivityID() {
+	return activityID;
+}
+public String getType() {
+	return type;
+}
+public int gettotalCCAdays() {
+	return totalCCAdays;
+}
+public int maxPax() {
+	return maxPax;
+}
+public String getStatus() {
+	return status;
+}
+public int getteacherID() {
+	return teacherID;
+}
+
+//activityid(int), type(string), totalCCAdays(int), maxPax(int), status(String, open/closed),teacherid(Int)
+}
